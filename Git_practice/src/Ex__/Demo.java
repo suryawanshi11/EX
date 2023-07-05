97b42ea6f0450fb642c7363166199a6992b23ed0
suryawanshi11 <133480937+suryawanshi11@users.noreply.github.com>
2023-07-05 23:54:38 +0530

    Update Demo.java
    add one method

--- Git_practice/src/Ex__/Demo.java
+++ Git_practice/src/Ex__/Demo.java
@@ -4,6 +4,12 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("done github topic");
+
+
+		public void m1()
+			{
+			System.out.println("update on github site");
+		}
 	}
 
 }
